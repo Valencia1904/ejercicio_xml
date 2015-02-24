72567d49b2e07f36e65aedbb10c3d3e38b3e351d
Alejandro Valencia Valles <Valencia1904@gmail.com>
2015-02-24 10:47:14 +0000

    Realizacion asta el ejercicio 4 pero falla el tipo de codigo

--- XML.py
+++ XML.py
@@ -14,7 +14,7 @@ for ayuda in ayudas:
 	print "---------------------------------------"
 
 print ""
-raw_input("Pulsa intro para iniciar ejercicio2")
+raw_input("Pulsa intro para iniciar ejercicio 2")
 os.system('clear')
 
 for ayuda in ayudas:
@@ -27,3 +27,33 @@ for ayuda in ayudas:
 		print ""
 				
 	print "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
+	
+print ""
+raw_input("Pulsa intro para iniciar ejercicio 3")
+os.system('clear')
+
+
+abierto=0
+cerrado=0
+for ayuda in ayudas:
+	if ayuda.findtext("estado")=="Cerrado":
+		cerrado=cerrado+1
+	else:
+		abierto=abierto+1
+
+print "Hay ",abierto," ayudas abiertas"
+print "Hay ",cerrado," ayudas cerradas"
+
+raw_input("Pulsa intro para iniciar ejercicio 4")
+os.system('clear')
+
+titulo=raw_input("Introduce el titulo de la beca que desea consultar: ")
+
+for ayuda in ayudas:
+	if str(ayuda.findtext("titulo"))==titulo:
+		print ayuda.findtext("titulo")
+		presentacion=ayuda.find("plazopresentacion")
+		plazopresentacion_item=presentacion.find("plazopresentacion_item")
+		print plazopresentacion_item.findtext("incial")
+		print plazopresentacion_item.findtext("final")
+		print ayuda.findtext("descripcion")
