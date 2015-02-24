bb3091d76fd934a81b6d9a01ac6f5a70b854af88
Alejandro Valencia Valles <Valencia1904@gmail.com>
2015-02-24 09:59:10 +0000

    Realizacion del primer ejercicio XML

--- XML.py
+++ XML.py
@@ -1,16 +1,14 @@
+# -*- coding: utf-8 -*-
+
 from lxml import etree
 xml=etree.parse("ayudas_0.xml")
 ayudas=xml.getroot()
 
-print ayudas[9].text
-#for i in xrange(len(ayudas)):
-#	print ayudas[i][2].text
-
-#for ayuda in ayudas:
-#	print ayuda.findtext("titulo")
-#	presentacion=ayuda.find("plazopresentacion")
-#	plazopresentacion_item=ayuda.find("plazopresentacion_item")
-#	print plazopresentacion_item.findtext("incial")
-#	print plazopresentacion_item.findtext("final")
-#	print ayuda.findtext("descripcion")
-#	print "---------------------------------------"
+for ayuda in ayudas:
+	print ayuda.findtext("titulo")
+	presentacion=ayuda.find("plazopresentacion")
+	plazopresentacion_item=presentacion.find("plazopresentacion_item")
+	print plazopresentacion_item.findtext("incial")
+	print plazopresentacion_item.findtext("final")
+	print ayuda.findtext("descripcion")
+	print "---------------------------------------"
