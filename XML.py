fcef63beb29e3391e35e303cbdc288b7fd6fc3f2
Alejandro Valencia Valles <Valencia1904@gmail.com>
2015-02-24 10:28:55 +0000

    Realizacion de ejercicio2 de xml

--- XML.py
+++ XML.py
@@ -1,5 +1,5 @@
 # -*- coding: utf-8 -*-
-
+import os
 from lxml import etree
 xml=etree.parse("ayudas_0.xml")
 ayudas=xml.getroot()
@@ -12,3 +12,18 @@ for ayuda in ayudas:
 	print plazopresentacion_item.findtext("final")
 	print ayuda.findtext("descripcion")
 	print "---------------------------------------"
+
+print ""
+raw_input("Pulsa intro para iniciar ejercicio2")
+os.system('clear')
+
+for ayuda in ayudas:
+	print ayuda.findtext("titulo")
+	print "- - - - - - - - - - - - - - - - - - - - - - - - - - - - "
+	documentacion=ayuda.find("documentacion")
+	
+	for documentacion_item in documentacion:
+		print documentacion_item.findtext("titulo")
+		print ""
+				
+	print "++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++"
